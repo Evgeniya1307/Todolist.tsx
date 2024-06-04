b64d3b6086f95a49d25cac2c4338b1a318a81460
Evgeniya13 <zxzxzx1307@gmail.com>
2024-06-04 15:06:31 -0600

    необязательное поле (указанное с помощью знака вопроса ?)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import { Todolist } from './Todolist';
 
@@ -8,28 +8,46 @@ type TaskType = {
   isDone: boolean;
 };
 
+export type FilterValuesType = 'all' | 'active' | 'completed';
+
 function App() {
-  const tasks1: Array<TaskType> = [
+  const [tasks, setTasks] = useState<TaskType[]>([
     { id: 1, title: 'HTML&CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'ReactJS', isDone: false },
     { id: 4, title: 'Redux', isDone: false },
     { id: 5, title: 'Typescript', isDone: false },
     { id: 6, title: 'RTK query', isDone: false },
-];
+  ]);
 
-const tasks2: Array<TaskType> = []; // Пустой массив задач для демонстрации "Тасок нет"
+  const [filter, setFilter] = useState<FilterValuesType>('all');
 
+  const removeTask = (taskId: number) => {
+    const filteredTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(filteredTasks);
+  };
+
+  const changeFilter = (filter: FilterValuesType) => {
+    setFilter(filter);//ожидает аргумент типа FilterValuesType ('all' | 'active' | 'completed')
+  };
+
+  let tasksForTodolist = tasks;
+
+  if (filter === 'active') {
+    tasksForTodolist = tasks.filter(task => !task.isDone);
+  }
+
+  if (filter === 'completed') {
+    tasksForTodolist = tasks.filter(task => task.isDone);
+  }
 
   return (
     <div className="App">
       <Todolist
         title="What to learn"
-        tasks={tasks1}
-      />
-      <Todolist
-        title="Songs"
-        tasks={tasks2}
+        tasks={tasksForTodolist}
+        removeTask={removeTask}
+        changeFilter={changeFilter}
       />
     </div>
   );
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,6 +1,5 @@
+import { FilterValuesType } from "./App";
 import { Button } from "./Button";
-
-
 
 type Task = {
   id: number;
@@ -11,9 +10,11 @@ type Task = {
 type PropsType = {
   title: string;
   tasks: Task[];
+  removeTask: (taskId: number) => void;
+  changeFilter: (filter: FilterValuesType) => void;
 };
 
-export const Todolist = ({ title, tasks }: PropsType) => {
+export const Todolist = ({ title, tasks, removeTask, changeFilter }: PropsType) => {
   return (
     <div className="todolist">
       <h3>{title}</h3>
@@ -29,16 +30,21 @@ export const Todolist = ({ title, tasks }: PropsType) => {
             <li key={task.id}>
               <input type="checkbox" checked={task.isDone} readOnly />
               <span>{task.title}</span>
+              <button onClick={() => removeTask(task.id)}>x</button>
             </li>
           ))}
         </ul>
       )}
-      <div className="buttons">
-      <Button title={'All'} />
-        <Button title={'Active'} />
-        <Button title={'Completed'} />
+      <div>
+        <Button title={'All'} onClick={() => changeFilter('all')} />
+        <Button title={'Active'} onClick={() => changeFilter('active')} />
+        <Button title={'Completed'} onClick={() => changeFilter('completed')} />
       </div>
     </div>
   );
 };
 
+//добавлены кнопки, которые вызывают функцию changeFilter при клике, чтобы изменить состояние фильтра в App
+// кнопки вызывают changeFilter с правильным типом аргумента ('all', 'active', 'completed')
+// при клике на эту кнопку показывать alert с id таски, для которой была нажата кнопка. Для этого передадим в button коллбэк-функцию для onClick
+//<button onClick={() => alert(task.id)}>x</button>
